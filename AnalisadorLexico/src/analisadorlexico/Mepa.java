c878af3a7440817cfc74a8f799c129a8aa05f1a7
NOTE-LUIZ\LuizSergio <luizsergiorferreira@hotmail.com>
2018-06-12 17:04:29 -0300

    Analisador sintatico

---
+++ AnalisadorLexico/src/analisadorlexico/Mepa.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analisadorlexico;
+
+/**
+ *
+ * @author LuizSergio
+ */
+public class Mepa {
+
+    private String Rot;
+    private String instrucao;
+    private String K;
+
+    Mepa() {
+        Rot = "";
+        instrucao = "";
+        K = "";
+    }
+
+    /**
+     * @return the Rot
+     */
+    public String getRot() {
+        return Rot;
+    }
+
+    /**
+     * @param Rot the Rot to set
+     */
+    public void setRot(String Rot) {
+        this.Rot = Rot;
+    }
+
+    /**
+     * @return the instrucao
+     */
+    public String getInstrucao() {
+        return instrucao;
+    }
+
+    /**
+     * @param instrucao the instrucao to set
+     */
+    public void setInstrucao(String instrucao) {
+        this.instrucao = instrucao;
+    }
+
+    /**
+     * @return the K
+     */
+    public String getK() {
+        return K;
+    }
+
+    /**
+     * @param K the K to set
+     */
+    public void setK(String K) {
+        this.K = K;
+    }
+
+    public Mepa(String Rot, String instrucao, String K) {
+        this.Rot = Rot;
+        this.instrucao = instrucao;
+        this.K = K;
+    }
+
+    @Override
+    public String toString() {
+        return "Mepa{" + "Rot=" + Rot + ", instrucao=" + instrucao + ", K=" + K + '}';
+    }
+    
+}
